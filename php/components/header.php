25f6e5dcac0671c96c13cd379054d0b8d1291476
lvcas-ca <lucas_.caceres@hotmail.com.ar>
2023-09-26 01:10:45 -0300

    comenzando 2.0

---
+++ php/components/header.php
@@ -0,0 +1,72 @@
+
+        <div class="logo-top-nav-mobile text-center">
+            <img class="logo-mobile p-2" src="/img-web/LOGO-color-footer.png" alt="logo" width="150px">
+        </div>
+        <div class="pre-nav-container">
+            <div class="pre-nav" id="inicio">
+            <div class="logo-container">
+               <a href="https://prezaserviciosintegrales.com"><img src="img-web/LOGO-color-footer.png" alt="Logo-preza"></a>
+            </div>
+                <div class="info">
+                    <div class="img-container">
+                      <img src="img-web/email-azul-oscuro.png" alt="">  
+                    </div>
+                    <div class="text-container-info">
+                       <span>Escribinos</span>
+                       <a href="mailto:mail@prezaserviciosintegrales.com?cc=preza.este@gmail.com">mail@prezaserviciosintegrales.com</a> 
+                    </div>
+                    
+                </div>
+
+                <div class="info">
+                     <div class="img-container">
+                        <img src="img-web/mapas-y-bander-azul.png" alt="Ubicacion">
+                     </div>
+                     <div class="text-container-info">
+                        <span>Ubicación</span>
+                        <a href="#">CABA | GBA</a>
+                     </div>
+                    
+                </div>
+
+                <div class="redes">
+                    <p>Seguinos:</p>
+                    <a href="https://www.facebook.com/profile.php?id=100085134056978" target="_blank"><img src="img-web/facebook-azul.png" alt="facebook"></a>
+                    <a href="https://www.instagram.com/preza_instalaciones/" target="_blank"><img src="img-web/instagram-azul.png" alt="instagram"></a>
+                </div>
+
+                <button class="hamburger hamburger--collapse" type="button">
+                    <span class="hamburger-box">
+                    <span class="hamburger-inner"></span>
+                    </span>
+                </button>
+
+            </div> 
+        </div>
+       
+        <nav class="menu menu-off">
+            <ul>
+               <li  class="">
+                  <a class="click" href="#inicio">Inicio</a>
+                  <div class="dropdown">
+                    <button class="btn btn-drop dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Servicios
+                    </button>
+                    <ul class="dropdown-menu">
+                      <li><a class="click dropdown-item" href="https://prezaserviciosintegrales.com/aire.html">Aire Acondicionado</a></li>
+                      <li><a class="click dropdown-item" href="https://prezaserviciosintegrales.com/electricidad.html">Electricidad</a></li>
+                      <li><a class="click dropdown-item" href="https://prezaserviciosintegrales.com/plomeria.html">Plomería</a></li>
+                      <li><a class="click dropdown-item" href="https://prezaserviciosintegrales.com/gas.html">Servicio de Gas</a></li>
+                      <li><a class="click dropdown-item" href="https://prezaserviciosintegrales.com/mantenimiento.html">Mantenimiento y reparación</a></li>
+                      <li><a class="click dropdown-item" href="https://prezaserviciosintegrales.com/camaras.html">Instalación Camaras de seguridad</a></li>
+                    </ul>
+                  </div>
+                  <a class="click" href="https://api.whatsapp.com/send?phone=5491159637882" target="_blank">Contacto</a>
+                  <a class="click" href="https://prezaserviciosintegrales.com/#nosotros">Nosotros</a>
+                  <a class="click" href="#emergencia">Emergencias</a>
+                  <a class="click enlace-trabaja" href="https://forms.gle/1xnkskvDbCvYcpDEA" target="_blank">Trabaja Con Nosotros</a>
+                </li>
+            </ul>
+        </nav>
+       
+  
